5b2dfba67754a07699a5a98ff2dd8ce13e84a4bb
moonhwi <moonhwi773@yahoo.com>
2019-07-03 15:54:09 +0800

    sss

--- scratch/backup_2.cc
+++ scratch/backup_2.cc
@@ -19,24 +19,6 @@ using namespace ns3;
 int
 main( int argc, char *argv[] )
 {
-
-    int AccessMatrix[15][15]={0};
-    for (int i = 0; i < 15; i++)
-    {
-       for (int j = 0; j < 15; j++)
-       {
-          AccessMatrix[i][j]=1;
-       }
-    }
-    for (int i = 0; i < 15; i++)
-    {
-       for (int j = 0; j < 15; j++)
-       {
-          std::cout<<AccessMatrix[i][j]<<" ";
-       }
-       std::cout<<std::endl;
-    }
-    
     //GlobalValue::Bind ("SimulatorImplementationType", StringValue ("ns3::RealtimeSimulatorImpl"));
     NodeContainer Satellite;
     Satellite.Create(15);
@@ -82,29 +64,6 @@ main( int argc, char *argv[] )
     wifi_2.SetStandard( WIFI_PHY_STANDARD_80211a );
     wifi_2.SetRemoteStationManager( "ns3::ConstantRateWifiManager", "DataMode", StringValue( "OfdmRate6Mbps" ) );
     NqosWifiMacHelper mac = NqosWifiMacHelper::Default();
-    //mac.SetType( "ns3::AdhocWifiMac",
- /*             //"Slot", StringValue( "16us" ) );
-    NodeContainer n0n1n2=NodeContainer(Satellite.Get(0),Satellite.Get(1),Satellite.Get(2));
-    NetDeviceContainer netn0n1n2=wifi.Install( spectrumPhy, mac, n0n1n2 );
-    NodeContainer n1n2n3=NodeContainer(Satellite.Get(1),Satellite.Get(2),Satellite.Get(3));
-    NetDeviceContainer netn1n2n3=wifi.Install( spectrumPhy, mac, n1n2n3 );
-    NodeContainer n3n4n5=NodeContainer(Satellite.Get(3),Satellite.Get(4),Satellite.Get(5));
-    NetDeviceContainer netn3n4n5=wifi.Install( spectrumPhy, mac, n3n4n5 );
-    NodeContainer n4n5n6=NodeContainer(Satellite.Get(4),Satellite.Get(5),Satellite.Get(6));
-    NetDeviceContainer netn4n5n6=wifi.Install( spectrumPhy, mac, n4n5n6 );
-    NodeContainer n6n7n8=NodeContainer(Satellite.Get(6),Satellite.Get(7),Satellite.Get(8));
-    NetDeviceContainer netn6n7n8=wifi.Install( spectrumPhy, mac, n6n7n8 );
-    NodeContainer n7n8n9=NodeContainer(Satellite.Get(7),Satellite.Get(8),Satellite.Get(9));
-    NetDeviceContainer netn7n8n9=wifi.Install( spectrumPhy, mac, n7n8n9 );
-    NodeContainer n9n10n11=NodeContainer(Satellite.Get(9),Satellite.Get(10),Satellite.Get(11));
-    NetDeviceContainer netn9n10n11=wifi.Install( spectrumPhy, mac, n9n10n11 );
-    NodeContainer n10n11n12=NodeContainer(Satellite.Get(10),Satellite.Get(11),Satellite.Get(12));
-    NetDeviceContainer netn10n11n12=wifi.Install( spectrumPhy, mac, n10n11n12 );
-    NodeContainer n12n13n14=NodeContainer(Satellite.Get(12),Satellite.Get(13),Satellite.Get(14));
-    NetDeviceContainer netn12n13n14=wifi.Install( spectrumPhy, mac, n12n13n14 );
-    NodeContainer n13n14n0=NodeContainer(Satellite.Get(13),Satellite.Get(14),Satellite.Get(0));
-    NetDeviceContainer netn13n14n0=wifi.Install( spectrumPhy, mac, n13n14n0 );
-*/
     NetDeviceContainer Satellitedevice;
     Satellitedevice = wifi.Install( spectrumPhy, mac, Satellite );
     NetDeviceContainer Satellitedevice_2;
@@ -141,56 +100,6 @@ main( int argc, char *argv[] )
     Satip = address.Assign(Satellitedevice);
     address.SetBase( "195.1.2.0", "255.255.255.0" );
     Ipv4InterfaceContainer Satip_1 = address.Assign(Satellitedevice_2);
-
-    
-    //address.SetBase( "195.1.2.0", "255.255.255.0" );
-    //Ipv4InterfaceContainer Satip_1 = address.Assign(Satellitedevice_2);
-/*   
-    address.SetBase( "195.1.1.0", "255.255.255.0" );
-    Ipv4InterfaceContainer Satip;
-    Satip = address.Assign(netn0n1n2);
-    address.SetBase( "195.1.2.0", "255.255.255.0" );
-    address.Assign(netn1n2n3);
-    address.SetBase( "195.1.3.0", "255.255.255.0" );
-    address.Assign(netn3n4n5);
-    address.SetBase( "195.1.4.0", "255.255.255.0" );
-    address.Assign(netn4n5n6);
-    address.SetBase( "195.1.5.0", "255.255.255.0" );
-    address.Assign(netn6n7n8);
-    address.SetBase( "195.1.6.0", "255.255.255.0" );
-    address.Assign(netn7n8n9);
-    address.SetBase( "195.1.7.0", "255.255.255.0" );
-    address.Assign(netn9n10n11);
-    address.SetBase( "195.1.8.0", "255.255.255.0" );
-    address.Assign(netn10n11n12);
-    address.SetBase( "195.1.9.0", "255.255.255.0" );
-    address.Assign(netn12n13n14);
-    address.SetBase( "195.1.10.0", "255.255.255.0" );
-    Ipv4InterfaceContainer Satip_1=address.Assign(netn13n14n0);
-*/
-   
-/*  OnOffHelper onOff1( "ns3::UdpSocketFactory", Address( InetSocketAddress( Satip.GetAddress(1), 9 ) ) );
-    Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (1024));
-    Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("448kb/s"));
-
-
-    onOff1.SetAttribute( "OnTime", StringValue( "ns3::ConstantRandomVariable[Constant=0.1]" ) );
-    onOff1.SetAttribute( "OffTime", StringValue( "ns3::ConstantRandomVariable[Constant=0.9]" ) );
-    ApplicationContainer apps1 = onOff1.Install( Satellite.Get(7) );
-    apps1.Start( Seconds( 1.0 ) );
-    apps1.Stop( Seconds( 30.0 ) );*/
-    
-
-
-/*  PacketSinkHelper sink ("ns3::UdpSocketFactory",
-    Address( InetSocketAddress( Satip.GetAddress( 7 ), 9)));
-    ApplicationContainer apps= sink.Install (Satellite);
-    apps.Start (Seconds (0));
-    apps.Stop (Seconds (50.0));
-    spectrumPhy.EnablePcapAll("try");*/    
-    
-    
-
     AnimationInterface anim( "/home/limemnghui/ns-allinone-3.27/ns-3.27/doublenetdevice/doubledevices.xml" );
     spectrumPhy.EnablePcapAll("/home/limemnghui/ns-allinone-3.27/ns-3.27/doublenetdevice/try_doublenetdevices");
     
